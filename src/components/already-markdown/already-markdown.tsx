0bfda1433cfdd8523d7fdf6007b9b281f0a5ac26
Sean Wong <seanwong24@live.com>
2021-10-05 16:40:19 -0600

    enable user-defined styles for markdown componenet

--- src/components/already-markdown/already-markdown.tsx
+++ src/components/already-markdown/already-markdown.tsx
@@ -1,5 +1,6 @@
-import { Component, Host, h, ComponentInterface, Prop } from '@stencil/core';
+import { Component, Host, h, ComponentInterface, Prop, Element } from '@stencil/core';
 import { parse } from "marked";
+import { observeMutation } from '../../utils/observe-mutation';
 
 @Component({
   tag: 'already-markdown',
@@ -8,14 +9,45 @@ import { parse } from "marked";
 })
 export class AlreadyMarkdown implements ComponentInterface {
 
+  private styleElement: HTMLStyleElement;
+
+  @Element() hostElement: HTMLAlreadyMarkdownElement;
+
   @Prop() src: string;
+
+  componentWillLoad() {
+    observeMutation.call(
+      this,
+      this.hostElement,
+      {
+        childList: true,
+        subtree: true
+      },
+      [
+        this.updateStyle
+      ]
+    );
+  }
+
+  componentDidRender() {
+    this.hostElement.shadowRoot.innerHTML = parse(this.src || '');
+    this.updateStyle();
+  }
 
   render() {
     return (
-      <Host>
-        <div innerHTML={parse(this.src || '')}></div>
-      </Host>
+      <Host></Host>
     );
+  }
+
+  private updateStyle() {
+    const templateElement = this.hostElement.querySelector('template');
+    const styleElement = templateElement?.content.querySelector('style');
+    this.styleElement?.remove();
+    this.styleElement = styleElement?.cloneNode(true) as HTMLStyleElement;
+    if (styleElement) {
+      this.hostElement.shadowRoot.appendChild(this.styleElement);
+    }
   }
 
 }
